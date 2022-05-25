095b803e05c5d059f34fc90d72515f2884bde366
maxgunchoi <chleorjs37@gmail.com>
2022-05-26 08:14:40 +0900

    마크다운 설치

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ChildComponent } from '@/app/component/child/child.component';
 
+//MarkdownModule
+import { MarkdownModule } from 'ngx-markdown';
+
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent
   ],
   imports: [
+    MarkdownModule.forRoot(),
     BrowserModule
   ],
   providers: [],
--- src/app/component/child/child.component.ts
+++ src/app/component/child/child.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent implements OnInit {
-
+  public markdown = `## Markdown __rulez__!
+  ---
+  
+  ### Syntax highlight
+  \`\`\`typescript
+  const language = 'typescript';
+  \`\`\`
+  
+  ### Lists
+  1. Ordered list
+  2. Another bullet point
+     - Unordered list
+     - Another unordered bullet
+  
+  ### Blockquote
+  > Blockquote to the max`;
   constructor() { }
 
   ngOnInit(): void {
